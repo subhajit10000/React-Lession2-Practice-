fab8871bad776a9b1c1e387b1096fb2b79cb1195
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-06-11 09:59:39 +0530

    Add files via upload

---
+++ MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+const MainLayout = () => {
+    return (
+    <>
+        <Header/>
+        <main className="min-h-screen">
+           <Outlet/>
+        </main>
+        <Footer/>
+    </>
+    );
+}
+export default MainLayout;
