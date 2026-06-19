4fa11ad95a363e7754c6796e6e66bc16486f2c50
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-06-19 09:32:26 +0530

    Refactor MainLayout component structure
    Refactor MainLayout to use a div for layout structure and add flex properties.

--- MainLayout.jsx
+++ MainLayout.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-const MainLayout = () => {
-    return (
-    <>
-        <Header/>
-        <main className="min-h-screen">
-           <Outlet/>
-        </main>
-        <Footer/>
-    </>
-    );
-}
+const MainLayout = () => (
+  <div className="min-h-screen flex flex-col">
+    <Header />
+
+    <main className="flex-grow">
+      <Outlet />
+    </main>
+
+    <Footer />
+  </div>
+);
+export default MainLayout;
 export default MainLayout;
